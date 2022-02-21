6f803aba7575e7306b2260dce7ea41de849cfd90
grace sopha <gdsopha24@gmail.com>
2022-02-21 15:20:25 -0600

    Create layout for tutor payment

---
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/TutorPay.java
@@ -0,0 +1,14 @@
+package com.example.studenttutoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorPay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_pay);
+    }
+}
