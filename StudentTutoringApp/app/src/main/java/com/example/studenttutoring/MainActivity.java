5f758383c692be84c95fec060bd793feaca83b15
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-02-22 19:13:50 -0600

    Fixing bug

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.studenttutoring.tutorpage.TutorPage;
+import com.example.studenttutoring.tutorpage.TutorPageViewModel;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
