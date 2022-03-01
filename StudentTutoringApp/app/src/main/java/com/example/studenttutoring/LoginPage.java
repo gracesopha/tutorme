9d81d48c30020f010ae6b775882069c5f5ce856d
lyang-86 <reallucy2002@gmail.com>
2022-03-01 16:48:09 -0600

    linked login page to launch

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/LoginPage.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/LoginPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +19,22 @@ public class LoginPage extends AppCompatActivity {
 
     public static void main (String[] args) {
         new LoginPage();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        final Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
