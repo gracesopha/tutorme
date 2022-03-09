cb945a68158863070ae522a4ffc58d27edc2c86f
SPatel29 <sunnypatelc9@gmail.com>
2022-03-08 21:02:08 -0600

    got input from user and added it to the database. Works

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/Ratings.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/Ratings.java
@@ -6,16 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Ratings extends AppCompatActivity {
 
     private Button submit_button;
     private Button cancel_button;
     private EditText name;
-
+    private EditText comment;
+    private Switch reccomend_switch;
+    private boolean reccomend = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,8 @@ public class Ratings extends AppCompatActivity {
         name = findViewById(R.id.editTextTextPersonName);
         submit_button = findViewById(R.id.submit_button);
         cancel_button = findViewById(R.id.cancel_button);
+        comment = findViewById(R.id.editTextTextMultiLine);
+        reccomend_switch = findViewById(R.id.switch2);
 
         submit_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) { submit(); }
@@ -35,6 +47,14 @@ public class Ratings extends AppCompatActivity {
                 //openActivity2();
             }
         });
+
+        reccomend_switch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                reccomend = true;
+            }
+        });
+
     }
 
     public void goHome() {
@@ -54,6 +74,29 @@ public class Ratings extends AppCompatActivity {
             AlertDialog dialog = builder.create();
             dialog.show();
             return;
+        }
+        else{
+            //get data from text fields
+            Connection conn;
+            String comment_text = comment.getText().toString();
+            String name_text = name.getText().toString();
+            int ratings_value = (reccomend) ? (1) : (0);
+            //Log.v("comment_text:",comment_text);
+            //Log.v("name_text", name_text);
+            //Log.v("recommend:", String.valueOf(reccomend));
+            try {
+                ConnectionHelper connectionHelper = new ConnectionHelper();
+                conn = connectionHelper.connectionclass();
+                Statement stmt = conn.createStatement();
+                String add = "";
+                add = "INSERT INTO REVIEW VALUES ('"+name_text+"', '"+comment_text+"', '"+ratings_value+"')";
+                stmt.executeUpdate(add);
+                Log.v("connection test:",comment_text);
+            }
+            catch (Exception ex){
+                Log.e("Error",ex.getMessage());
+            }
+
         }
         startActivity(intent);
         finish();
