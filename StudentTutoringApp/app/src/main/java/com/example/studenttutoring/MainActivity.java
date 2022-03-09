3865f8e5becc0ec1aa8897f58f65d5f5e1631814
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-03-08 20:26:27 -0600

    adding logout button function on TutorPage.java

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.studenttutoring;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 
 import android.content.Intent;
--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/tutorpage/TutorPage.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/tutorpage/TutorPage.java
@@ -2,6 +2,8 @@ package com.example.studenttutoring.tutorpage;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.studenttutoring.LoginPage;
 import com.example.studenttutoring.MainActivity;
 import com.example.studenttutoring.R;
+import com.example.studenttutoring.SignupPage;
 
 public class TutorPage extends Fragment {
     private static final String TAG = "TutorPage";
@@ -23,6 +28,8 @@ public class TutorPage extends Fragment {
     private TutorPageViewModel mViewModel;
     private TextView email;
     String userEmail = "abc@gmail.com";
+    Button logoutButton;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -33,6 +40,10 @@ public class TutorPage extends Fragment {
         email.setText(userEmail);
         Log.d(TAG, "TutorPage : pulled string "+userEmail);
         Log.d(TAG, "TutorPage : Email string "+email.getText().toString());
+
+        // Logout button
+        logoutButton = (Button) v.findViewById(R.id.logout);
+
         return v;
     }
 
@@ -41,6 +52,13 @@ public class TutorPage extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(TutorPageViewModel.class);
         // TODO: Use the ViewModel
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), LoginPage.class);
+                startActivity(i);
+                ((Activity) getActivity()).overridePendingTransition(0,0);
+            }
+        });
     }
-
 }
