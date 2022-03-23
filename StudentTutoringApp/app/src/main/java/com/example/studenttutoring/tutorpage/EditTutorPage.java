0967315ca6ceb9d4a7e3ac86ee951cd46e98976e
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-03-22 20:53:16 -0500

    changing error on EditTutorPage.java

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/tutorpage/EditTutorPage.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/tutorpage/EditTutorPage.java
@@ -9,6 +9,5 @@ public class EditTutorPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_tutor_page);
     }
 }
