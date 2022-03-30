9dca9f398000f19c20c1d183bd016ea44988c860
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-03-29 20:42:14 -0500

    adding the save button function for both tutor and student

--- StudentTutoringApp/app/src/main/java/com/example/studenttutoring/EditTutorPage.java
+++ StudentTutoringApp/app/src/main/java/com/example/studenttutoring/EditTutorPage.java
@@ -2,14 +2,30 @@ package com.example.studenttutoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditTutorPage extends AppCompatActivity {
 
+    private Button save_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_tutor_page);
+
+        // Save Button
+        save_button = findViewById(R.id.save_button);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(EditTutorPage.this, MainActivityTutor.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
